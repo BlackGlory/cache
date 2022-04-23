28702f933d81cef40f3fbf6085efcd51f99df394
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-04-23 03:31:26 +0000

    feat: add logging

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import { WS_HEARTBEAT_INTERVAL } from '@env/index.js'
+import { WS_HEARTBEAT_INTERVAL, NODE_ENV, NodeEnv } from '@env/index.js'
 import { API } from './apis/index.js'
 import path from 'path'
 import appRoot from 'app-root-path'
 import { readJSONFileSync } from 'extra-filesystem'
-import { createServer } from '@delight-rpc/websocket'
+import { createServer, Level } from '@delight-rpc/websocket'
 import { WebSocket, WebSocketServer } from 'ws'
 import { Destructor } from 'extra-defer'
 import { setDynamicTimeoutLoop } from 'extra-timers'
@@ -22,7 +22,10 @@ export function startServer(host: string, port: number): () => Promise<void> {
     sockets.add(socket)
     destructor.defer(() => sockets.delete(socket))
 
-    const close = createServer(API, socket, undefined, pkg.version)
+    const close = createServer(API, socket, {
+      loggerLevel: NODE_ENV() === NodeEnv.Test ? Level.None : Level.Info
+    , version: pkg.version
+    })
     destructor.defer(() => close())
 
     const cancelHeartbeatTimer: (() => void) | null = WS_HEARTBEAT_INTERVAL() > 0
