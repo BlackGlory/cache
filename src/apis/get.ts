6542c3bd943abfaf476972021a4d7f8d5507fddd
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-08-01 09:14:48 +0000

    chore: fix typo

--- src/apis/get.ts
+++ src/apis/get.ts
@@ -2,8 +2,8 @@ import { createInternalKey } from '@utils/internal-key.js'
 import { isUndefined } from '@blackglory/prelude'
 import { cache } from '@src/cache.js'
 
-export function get(namespace: string, keyd: string): string | null {
-  const internalKey = createInternalKey(namespace, keyd)
+export function get(namespace: string, key: string): string | null {
+  const internalKey = createInternalKey(namespace, key)
   const item = cache.get(internalKey)
   if (isUndefined(item)) return null
   return item.value.toString()
